e47f294f92e5b3d60ab49c49f3ca0fc3f46dea79
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-08 22:41:51 +0800

    Update streamlit_app.py
    upg catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,8 @@ categories = {
         "flag masih rg7",
         "ru pemasangan pakai rg6",
         "order dh siap nk done kuar error guna wifi rg6 ax3000",
-        "tukar existing equipment combo box ke be7200 wifi 7 ke combo box ax3000"
+        "tukar existing equipment combo box ke be7200 wifi 7 ke combo box ax3000",
+        "tukar new equipment dari rg7 ke combo rg6"
     ],
     "Update Order New/ Existing Equipment Info": [
         "ubah equipment ke rg6",
