9a25bb264f77b610b9afaa78947a3324598bdccb
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-09 21:53:58 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -238,7 +238,8 @@ categories = {
         "ctt 1-2 no slot",
         "tt no slot",
         "mohon bantuan tt no slot 1-116330643385, tq",
-        "assalammualaikum  mohon bantu appointment error (400). tq  1-116374056595."
+        "assalammualaikum  mohon bantu appointment error (400). tq  1-116374056595.",
+        "appointment error (400)"
     ],
     "TT HSBA Reappointment": [
         "bantuan untuk patchkan ctt tq 1-116312148935 - appt on 10/9/25 at 11.30am",
