90fb17ee9c1f4dff64fe60c0a220af3d8536e6c7
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-09 21:23:32 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,8 @@ categories = {
         "1-115109708172    mohon bantu tukar cpe kepada rg6",
         "2509000080219214 | isu fail update equipment  mohon bantuan untuk update cpe wifi 7 ke wifi6 ax3000 combo.  no order: 2509000080219214 s/n no 1: unc25val2412025438 s/n no 2 mesh: unc25val2412025439",
         "tma q104706 1-113191017898 mntk tlg tukar eq ke combo biasa, order schoolnet. tq",
-        "mohon bantuan tukarkan equipment rg7 ke rg6 ax3000 router & mesh....order modify 1gb  2509000080869301"
+        "mohon bantuan tukarkan equipment rg7 ke rg6 ax3000 router & mesh....order modify 1gb  2509000080869301",
+        "mohon bantuan flag masih rg7 ru x lepas nak donekan order..cust sudah nak keluar rumah  1-115865565435"
     ],
     "New/ Existing/ Delete Equipment Info Update": [
         "semakan order upgrade pakej 2gb tp dlm tmf equipment yg new vm shj",
