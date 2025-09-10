ab7c3d7622465f1ae2e6842b09f5b417e3fe375f
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-10 18:57:31 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -254,7 +254,8 @@ categories = {
         "mohon bantuan @en md rad mohon bantuan ctt no :1-116321367562 no siri lama :vdlinnne1911037019 no siri baru  :unc30val250134169",
         "salam bantuan team tak boleh tutup ctt tk boleh swap cpe  team tukar combo box  ctt no 1-116375717449",
         "ctt tk boleh swap cpe  team tukar combo box",
-        "ada error untuk done  1-116336590617  sn baru : uncfh5f32502020536 sn lama : rg6wfh1y220701631"
+        "ada error untuk done  1-116336590617  sn baru : uncfh5f32502020536 sn lama : rg6wfh1y220701631",
+        "ada error untuk done"
     ],
     "TT V1P": [
         "assalamualaikum..tt v1p 1-26814783348 appt semula hari ni 10.30 am..tq",
