633943316d9695e103c464e75e03279b600ccf09
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-11 19:27:40 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,8 @@ categories = {
         "tukarkan  8 dect (new cpe) kpd existing krn cstmer guna pabx tq",
         "minta tukar btu ke new..order x share btu",
         "boleh check ini equipment sdh add 1 ata pun masihh sama",
-        "oder. ni 1gb tp tukar modem sj btolke"
+        "oder. ni 1gb tp tukar modem sj btolke",
+        "jdkan new btu dekat equipment..order relocate sebelum ini cust pakai copper (sbvm)"
     ],
     "Update Order Equipment Details": [
         "nk done keluar error seperti berikut..tq  1-116069040600  router-rg6dlax32311015973 mesh rg6-rg6dlax32311015977",
