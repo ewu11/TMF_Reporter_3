2b2b0d60f849eb11bd4a64c03767d2f6440a7ea9
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-13 17:50:43 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -265,7 +265,8 @@ categories = {
         "mntk tlg tngk order dah done tpi masih in progress",
         "jadikan order kepada status completed sebab pihak vasp daripada digi confirm order telah completed di fizikal...dlm tmf ui tidak boleh done sebab semua activity sudah completed",
         "1-115390711719-mohon bantuan check order ni minta tlg cek dh completed tp masih inprogress",
-        "tlg cek dh completed tp masih inprogress"
+        "tlg cek dh completed tp masih inprogress",
+        "ui tak boleh donekan order"
     ],
     "Revert BAU SWIFT-TMF Order": [
         "1-cbga8gd | assalam team, mohon revert order id dibawah ke tmf system. urgent!!! tq 1-cbga8gd 1-cbgnu4p",
