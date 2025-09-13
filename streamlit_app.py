fbbed4a9d718c34b0dcd99947d039b7d646b67bd
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-13 18:02:57 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -507,7 +507,8 @@ categories = {
         "team xboleh update cpe..",
         "x dapt close ctt untuk pertukaran  cpe..dlm tm force cpe list btu tiada..team nak scan btu tak boleh",
         "verify, customer ada langgan mesh sekali ke ? sebab team inform di fizikal ada mesh tetapi system tiada",
-        "delete equipment mesh, pelanggan maklum tidak pakai mesh.. fizikal pon tiada mesh hanya ada 1 router sahaja"
+        "delete equipment mesh, pelanggan maklum tidak pakai mesh.. fizikal pon tiada mesh hanya ada 1 router sahaja",
+        "1-116541338489 mohn bantuan verify ctt ni vdsl ke atau fiber sebab dekat equipmnt tmf ada modem dekat site tiada modem 🙏🏻"
     ],
     "TT - LR Linkage": [
         "salam team bantuan clear tmf ctt link lr20250312-39719 1-106191315217 1-106194182478 1-106312056435 1-106312232721 1-106346917798 1-106471083514 1-106678032670 1-109407463175  tq",
