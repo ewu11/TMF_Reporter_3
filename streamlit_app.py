22c23dc3be2ff59043f8d0a2345ad2f0b8394912
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-15 18:57:57 +0800

    Update streamlit_app.py
    upd bias

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ def apply_bias(msg: str, scores: dict) -> dict:
     """
     text = msg.lower()
 
-    if "tiada slot/no slot" in text:
-        if "tt" in text:
+    if "tt" in text:
+        if "tiada slot/no slot" in text:
             scores["TT Error 400"] = scores.get("TT Error 400", 0) + 0.5
             scores["Order Next Activity Not Appear"] = scores.get("Order Next Activity Not Appear", 0) - 0.5
 
