02430793f9a6ea78a6d5601b7acbfea32b1d63bc
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-17 23:01:11 +0800

    Update streamlit_app.py
    up bias

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def apply_bias(msg: str, scores: dict) -> dict:
     if "order dah refresh network tp failed" in text:
         scores["Update Granite Network Details"] = scores.get("Update Granite Network Details", 0) + 0.3
 
-    if "Order Next Activity Not Appear" in text:
+    if "tiada next owner" in text:
         scores["Order Next Activity Not Appear"] = scores.get("Order Next Activity Not Appear", 0) + 0.3
 
     # Cap scores between 0.0 and 1.0
