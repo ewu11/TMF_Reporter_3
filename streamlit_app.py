7b25338eb69578b07452dcc264af32480289a33b
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-17 23:02:39 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def apply_bias(msg: str, scores: dict) -> dict:
         scores["Update Granite Network Details"] = scores.get("Update Granite Network Details", 0) + 0.3
 
     if "tiada next owner" in text:
-        scores["Order Next Activity Not Appear"] = scores.get("Order Next Activity Not Appear", 0) + 0.3
+        scores["Order Next Activity Not Appear"] = scores.get("Order Next Activity Not Appear", 0) + 0.2
 
     # Cap scores between 0.0 and 1.0
     scores = {k: max(0.0, min(v, 1.0)) for k, v in scores.items()}
@@ -605,6 +605,9 @@ categories = {
     "TT - Activity Work Type Blank": [
         "mohon bantuan...ctt tiada activity work type...ctt no : 1-115880486755..iris sudah closed minta retry...tq",
         "xtvt worktype xd. zon nsj. tq"
+    ],
+    "User Management Issues": [
+        "ru inform id problem tak dapat masuk tmf q004560"
     ]
 }
 
