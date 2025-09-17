eb480292367873954ebe350345b3566cb9198507
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-17 23:54:36 +0800

    Update streamlit_app.py
    upd bas

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,11 +68,12 @@ def apply_bias(msg: str, scores: dict) -> dict:
         scores["New/ Existing/ Delete Equipment Info Update"] = scores.get("New/ Existing/ Delete Equipment Info Update", 0) - 0.1
 
     if ("valid rg" in text):
-        scores["TT RG6/ Combo Update]"] = scores.get("TT RG6/ Combo Update]", 0) + 0.2
+        scores["TT RG6/ Combo Update]"] = scores.get("TT RG6/ Combo Update]", 0) + 0.3
         scores["RG6 - RG7 Equipment Info Update"] = scores.get("RG6 - RG7 Equipment Info Update", 0) - 0.1
         scores["Update Order Equipment Details"] = scores.get("Update Order Equipment Details", 0) - 0.1 
         scores["New/ Existing/ Delete Equipment Info Update"] = scores.get("New/ Existing/ Delete Equipment Info Update", 0) - 0.1 
         scores["TT TMF-Physical CPE Unsync"] = scores.get("TT TMF-Physical CPE Unsync", 0) - 0.1
+        scores["Release Assign to Me"] = scores.get("Release Assign to Me", 0) - 0.1
     
     if "order dah refresh network tp failed" in text:
         scores["Update Granite Network Details"] = scores.get("Update Granite Network Details", 0) + 0.3
