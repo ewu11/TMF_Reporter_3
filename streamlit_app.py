618fa445a80d0f8f3fc22087934fc4dfab57bc43
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-17 23:26:54 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def apply_bias(msg: str, scores: dict) -> dict:
     """
     text = msg.lower()
 
-    if "tt" in text:
+    if "tt" in text or "ctt" in text:
         if "tiada slot" in text or "no slot" in text:
             scores["TT Error 400"] = scores.get("TT Error 400", 0) + 0.1
             scores["Order Next Activity Not Appear"] = scores.get("Order Next Activity Not Appear", 0) - 0.1
@@ -33,7 +33,7 @@ def apply_bias(msg: str, scores: dict) -> dict:
             scores["TT RG6/ Combo Update"] = scores.get("TT RG6/ Combo Update", 0) + 0.1
             scores["New/ Existing/ Delete Equipment Info Update"] = scores.get("New/ Existing/ Delete Equipment Info Update", 0) - 0.1
 
-    if "order" in text:
+    if "order" in text or "oder" in text:
         if "tukar equipment ke combo ax3000" in text or "customer package" in text:
             scores["RG6 - RG7 Equipment Info Update"] = scores.get("RG6 - RG7 Equipment Info Update", 0) + 0.1
             scores["New/ Existing/ Delete Equipment Info Update"] = scores.get("New/ Existing/ Delete Equipment Info Update", 0) - 0.1
