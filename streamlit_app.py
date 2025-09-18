981d2baabea95837513cfa7ec258299967a41979
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-18 22:09:55 +0800

    Update streamlit_app.py
    upd bias

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def apply_bias(msg: str, scores: dict) -> dict:
         if "ra" in text and ("dalam" in text or "dlm" in text) and ("rtn" in text or "return" in text or "returned" in text):
             scores["Order Next Activity Not Appear"] = scores.get("Order Next Activity Not Appear", 0) + 0.1
             scores["Order Missing/ Pending Processing"] = scores.get("Order Missing/ Pending Processing", 0) - 0.1
-        if ("sn:" in text or "sn" in text or "s/n" in text or "s\/n" in text or "s/n:" in text) and ("ctc" not in text or "contact" not in text):
+        if ("sn:" in text or "sn" in text or "s/n" in text or "s\/n" in text or "s/n:" in text or "s\/n:" in text) and ("ctc" not in text or "contact" not in text):
             st.write(":D")
             scores["RG6 - RG7 Equipment Info Update"] = scores.get("RG6 - RG7 Equipment Info Update", 0) + 0.2
             scores["Update Contact Number"] = scores.get("Update Contact Number", 0) - 0.3
