0285eda61b4bfc4a87d04d3c30532145430b72f8
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-18 22:33:15 +0800

    Update streamlit_app.py
    upd bias

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,8 +66,10 @@ def apply_bias(msg: str, scores: dict) -> dict:
         if "vdsl" in text or "ftth" in text:
             scores["New/ Existing/ Delete Equipment Info Update"] = scores.get("New/ Existing/ Delete Equipment Info Update", 0) + 0.2
             scores["TT RG6/ Combo Update"] = scores.get("TT RG6/ Combo Update", 0) - 0.1
-        if ("ma" in text or "manual assign" in text) and ("btn" in text or "butang" in text or "button" in text):
+        if ("ma" in text or "manual assign" in text):
             scores["Manual Assign Button not Appear"] = scores.get("Manual Assign Button not Appear", 0) + 0.1
+            ("btn" in text or "butang" in text or "button" in text)
+                scores["Manual Assign Button not Appear"] = scores.get("Manual Assign Button not Appear", 0) + 0.3
     
     if "tukar kan equipment ke existing" in text or "hanya tambah fixed ip bukan tukar brg" in text:
         scores["New/ Existing/ Delete Equipment Info Update"] = scores.get("New/ Existing/ Delete Equipment Info Update", 0) + 0.1
