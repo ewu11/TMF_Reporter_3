c5747439dc1716b9e2f671f13082afb66e135ef1
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-20 00:46:09 +0800

    Update streamlit_app.py
    upd catg

--- streamlit_app.py
+++ streamlit_app.py
@@ -453,7 +453,8 @@ categories = {
         "terus ke saf..x sempat scan cpe lg, tq",
         "ui dah complete order tapi masih x hilang. ui xleh nak buat next order. tk",
         "2509000081788103 bantuan ui blm sempat scan cpe ..order sudah status completed",
-        "team baru buat hsi .. terus auto done order.."
+        "team baru buat hsi .. terus auto done order..",
+        "order xdan scan barcode sn dah skip pi ke saf .."
     ],
     "TT RG5 Equipment Update": [
         "cpe rg5 & onu ke new router combo",
