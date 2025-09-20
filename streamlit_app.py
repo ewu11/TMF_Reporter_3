e1a30b4b9e5af54228a93ed65cae6bee505954c1
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-20 14:32:56 +0800

    Update streamlit_app.py
    upd bisa

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,6 +116,7 @@ def apply_bias(msg: str, scores: dict) -> dict:
         if re.search(r"\b1-2\d{10,11}\b", text) or re.search(r"slot.*id", text):
             scores["TT V1P"] = scores.get("TT V1P]", 0) + 0.2
             scores["TT HSBA Reappointment"] = scores.get("TT HSBA Reappointment", 0) - 0.1
+            scores["TT Unsync"] = scores.get("TT Unsync", 0) - 0.1
     
     # if "cpe" in text:
     #     scores["TT RG6/ Combo Update"] = scores.get("TT RG6/ Combo Update", 0) + 0.2
