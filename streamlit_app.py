b0ae7ff9c1afbf14654a9035452877e2f406c607
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-22 18:54:20 +0800

    Update streamlit_app.py
    upd escape sequence \/

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ def apply_bias(msg: str, scores: dict) -> dict:
         if "ra" in text and ("dalam" in text or "dlm" in text) and ("rtn" in text or "return" in text or "returned" in text):
             scores["Next Order Activity Not Appear"] = scores.get("Next Order Activity Not Appear", 0) + 0.1
             scores["Order Missing/ Pending Processing"] = scores.get("Order Missing/ Pending Processing", 0) - 0.1
-        if ("sn:" in text or "sn" in text or "s/n" in text or "s\/n" in text or "s/n:" in text or "s\/n:" in text) and ("ctc" not in text or "contact" not in text):
+        if ("sn:" in text or "sn" in text or "s/n" in text or r"s\/n" in text or "s/n:" in text or r"s\/n:" in text) and ("ctc" not in text or "contact" not in text):
             scores["RG6 - RG7 Equipment Info Update"] = scores.get("RG6 - RG7 Equipment Info Update", 0) + 0.2
             scores["Update Contact Number"] = scores.get("Update Contact Number", 0) - 0.3
         if ("equipment" in text or "eqp" in text or "eqmnt" in text) and "vendor" in text:
