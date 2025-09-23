28e3d6105a914f9228580be9d33056116130ae4d
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-23 21:41:31 +0800

    Update streamlit_app.py
    upd bias

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,6 +72,9 @@ def apply_bias(msg: str, scores: dict) -> dict:
         if (re.search(r"cpe.*swap.*serial number.*exist", text)):
             scores["TT RG6/ Combo Update"] = scores.get("TT RG6/ Combo Update", 0) + 0.2
             scores["TT Error 400"] = scores.get("TT Error 400", 0) - 0.1
+        if (re.search(r"del(.)?(.)?(.)?", text) and re.search(r"(.)?d(c|p)( id)?", text)):
+            scores["Update Granite Network Details"] = scores.get("Update Granite Network Details", 0) + 0.2
+            scores["Invalid ICBRN Number"] = scores.get("Invalid ICBRN Number", 0) - 0.1
 
     if "order" in text or "oder" in text:
         if "tukar equipment ke combo ax3000" in text or "customer package" in text:
