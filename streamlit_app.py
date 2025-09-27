8444756b8295b878564ccedc1229f3c58931f18d
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2025-09-27 21:55:02 +0800

    Update streamlit_app.py
    upd bias

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,6 +78,10 @@ def apply_bias(msg: str, scores: dict) -> dict:
         if (re.search(r"(f)?d(c|p)?", text) or re.search(r"cab(inet)?", text)) and re.search(r"map(ping)?", text):
             scores["TT Error 400"] = scores.get("TT Error 400", 0) + 0.2
             scores["Update Granite Network Details"] = scores.get("Update Granite Network Details", 0) - 0.2
+        if ("no slot" in text):
+            scores["TT Error 400"] = scores.get("TT Error 400", 0) + 0.2
+            scores["Update Order Equipment Details"] = scores.get("Update Order Equipment Details", 0) - 0.1
+            scores["Order Capping Issue"] = scores.get("Order Capping Issue", 0) - 0.1
 
     if "order" in text or "oder" in text:
         if "tukar equipment ke combo ax3000" in text or "customer package" in text:
